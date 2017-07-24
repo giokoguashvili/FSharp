b7bff7adddc9f9b99dd82bc1d30dd209ca0a09ff
Giorgi Koguashvili <kogo27@gmail.com>
2017-07-24 12:41:23 +0400

    commit

---
+++ Test2/StringsWithIdenticalLetters.fs
@@ -0,0 +1,22 @@
+﻿module StringsWithIdenticalLetters
+
+open System
+
+let strings = ["abc"; "bac"; "abc"; "d"; "et"; "d"; "et"]
+
+
+let sortedString (str:string) =
+    str 
+    |> Seq.sort
+    |> String.Concat
+
+let stringsWithSortedLetters strs =
+    strs |> List.map sortedString
+
+let stringsWithIdenticalLetters strs =
+    let startState = (List.head strs, 0)
+    strs 
+    //|> List.fold(
+
+printfn "%A" <| stringsWithSortedLetters strings
+
---
+++ TestProject/NewLineFeature.fs
@@ -0,0 +1,49 @@
+﻿module NewLineFeature
+
+type C(num) =
+    member this.Add(x) = 
+        num + x 
+  
+type B(c:C) =
+    member this.Add(x) = c.Add(x)
+
+type A(b:B) =
+    member this.Add(x) = b.Add(x)
+
+let add1 = (+) 1
+let prod2 = (*) 2
+let div3 = (/) 3
+
+
+
+let result x = (add1 >> prod2 >> div3) x
+let result1 x = (add1 << prod2 << div3) x
+let result2 x = add1  (prod2  (div3 x))
+//let result3 x = 
+//        add1
+//            prod2
+//                div3 x
+
+let dec x = 
+        A(
+            B(
+                C(1)
+            )
+        ).Add(x)
+
+//let result =
+//        ParsedXML
+//            XMLFile "path"
+
+
+//let dec1 x = 
+//        A
+//            B
+//                C 1    
+//        Add(x)
+
+printf "%A" <| dec 5                    
+
+printfn "%A" <| result 3
+printfn "%A" <| result1 3
+printfn "%A" <| result2 3
