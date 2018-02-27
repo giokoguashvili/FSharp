962e008105c57be7a280049459c626ea6fbb2042
kogoia <kogo27@gmail.com>
2018-02-27 03:11:02 -0800

    removed C# code

--- ParserCombinators/Models1/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Models1
-{
-    public class User
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public decimal Salary { get; set; }
-    }
-}
